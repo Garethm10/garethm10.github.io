3fe13aa256a39d3b3d5c7e5bbe4c7c8afdac39f5
Garethm10 <gareth.meredith@utexas.edu>
2022-12-21 09:10:28 -0600

    please

--- static/Calculator.js
+++ static/Calculator.js
@@ -1,7 +1,9 @@
+
+
 function myFunc(){
     var name = document.getElementById('input-name').value;
     var cats = document.getElementById('input-CatIntake').value;
     var dogs = document.getElementById('input-DogIntake').value;
      alert("You're a silly goober named " + name + " and there are " + (dogs - cats) + " more dogs than cats" );
-     document.getElementById('output-MoreDogsThanCats').value = (dogs - cats)
+     document.getElementById('output-MoreDogsThanCats').value = (dogs - cats);
   }
